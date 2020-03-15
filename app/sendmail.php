2e7488393d04dcf4fe76c641d46f097e7f92c465
Ilya Skvortsov <ilay101096@yandex.ru>
2020-03-15 22:15:37 +0300

    first commit

---
+++ app/sendmail.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+	require("class.phpmailer.php");
+
+	$mail = new PHPMailer;
+
+	$body="";
+	$subject = "Заявка с сайта " . $_SERVER['SERVER_NAME'];
+	$body = "Поступила заявка :<br/>";
+	$body .= "<b>Имя:</b> ".$_POST["name"]."<br/>";
+	$body .= "<b>Телефон:</b> ".$_POST["phone"]."<br/>";
+
+	$mail->IsSendmail();
+	$mail->IsHTML(true); 
+	$mail->FromName = 'К НАМ';
+	$mail->AddAddress('smirnov@dadcan.ru');
+	$mail->CharSet = 'utf-8';
+	$mail->Subject = $subject;
+	$mail->Body    = $body;
+
+	if(!$mail->Send()) {
+	   
+	}
+	
+?>
